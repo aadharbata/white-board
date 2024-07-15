eb87a6dde91c1754c387c68b96bbaa72918f443f
asr-orzz <143619867+asr-orzz@users.noreply.github.com>
2024-07-15 15:32:14 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
-import { FaSlash, FaRegCircle, FaLongArrowAltRight,FaPaintBrush } from 'react-icons/fa';
+import { FaSlash, FaRegCircle, FaLongArrowAltRight, FaPaintBrush, FaEraser } from 'react-icons/fa';
 import { LuRectangleHorizontal } from 'react-icons/lu';
-import { FaEraser } from 'react-icons/fa6';
 import { MdOutlineFormatColorText } from "react-icons/md";
+
 const toolItems = [
     { item: 'Pencil', element: FaPaintBrush },
     { item: 'Line', element: FaSlash },
@@ -9,28 +9,31 @@ const toolItems = [
     { item: 'Circle', element: FaRegCircle },
     { item: 'Arrow', element: FaLongArrowAltRight },
     { item: 'Eraser', element: FaEraser },
-    {item: 'Text', element:MdOutlineFormatColorText }
-  ];
+    { item: 'Text', element: MdOutlineFormatColorText }
+];
 
 export default toolItems;
 
-export const STROKE_ACTIONS=["Line","Rectangle","Circle","Arrow","Pencil","Text"];
+export const STROKE_ACTIONS = ["Line", "Rectangle", "Circle", "Arrow", "Pencil", "Text"];
 
-export const FILL_COLOR=["Rectangle","Circle"];
-export const boardActions={
-     MOUSE_DOWN:"MOUSE_DOWN",
-     MOUSE_UP: "MOUSE_UP",
-     CHANGE_TOOL:"CHANGE_TOOL"
+export const FILL_COLOR = ["Rectangle", "Circle"];
+
+export const boardActions = {
+    MOUSE_DOWN: "MOUSE_DOWN",
+    MOUSE_UP: "MOUSE_UP",
+    CHANGE_TOOL: "CHANGE_TOOL"
 };
+
 export const COLORS = {
-  BLACK: "#000000",
-  RED: "#ff0000",
-  GREEN: "#00ff00",
-  BLUE: "#0000ff",
-  ORANGE: "#ffa500",
-  YELLOW: "#ffff00"
+    BLACK: "#000000",
+    RED: "#ff0000",
+    GREEN: "#00ff00",
+    BLUE: "#0000ff",
+    ORANGE: "#ffa500",
+    YELLOW: "#ffff00"
 };
 
-export const SIZE_TOOL=["Line","Rectangle","Circle","Arrow","Text","Pencil","Eraser"];
+export const SIZE_TOOL = ["Line", "Rectangle", "Circle", "Arrow", "Text", "Pencil", "Eraser"];
 
-export const TOOL_BOX=["Line","Rectangle","Circle","Arrow","Pencil","Text","Eraser"];
+export const TOOL_BOX = ["Line", "Rectangle", "Circle", "Arrow", "Pencil", "Text", "Eraser"];
+
